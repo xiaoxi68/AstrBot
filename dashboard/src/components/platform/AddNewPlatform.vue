524285f767b7e80eaaaa2405b6d1a87c98464066
Soulter <905617992@qq.com>
2025-09-27 20:41:45 +0800

    feat: add cancel button with localized text to AddNewPlatform and update close button in AddNewProvider
    fixes: #2889

--- dashboard/src/components/platform/AddNewPlatform.vue
+++ dashboard/src/components/platform/AddNewPlatform.vue
@@ -31,6 +31,10 @@
           </v-col>
         </v-row>
       </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn text @click="closeDialog">{{ tm('dialog.cancel') }}</v-btn>
+      </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
--- dashboard/src/components/provider/AddNewProvider.vue
+++ dashboard/src/components/provider/AddNewProvider.vue
@@ -63,9 +63,7 @@
             </v-card-text>
             <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn text @click="closeDialog">
-                    Close
-                </v-btn>
+                <v-btn text @click="closeDialog">{{ tm('dialogs.config.cancel') }}</v-btn>
             </v-card-actions>
         </v-card>
     </v-dialog>
